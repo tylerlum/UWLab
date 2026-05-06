e7db0b813aef21e04ea52e11d0746359fdcebea9
Tyler Lum <tylergwlum@gmail.com>
2026-05-06 04:55:38 -0700

    Avoid missing checkpoint symlink retry stalls

--- rl_games/rl_games/algos_torch/torch_ext.py
+++ rl_games/rl_games/algos_torch/torch_ext.py
@@ -72,8 +72,9 @@ def safe_filesystem_op(func, *args, **kwargs):
 
 def safe_symlink(src, dst):
     try:
-        safe_filesystem_op(os.remove, dst)
-    except (FileExistsError, RuntimeError):
+        if os.path.lexists(dst):
+            safe_filesystem_op(os.remove, dst)
+    except (FileExistsError, FileNotFoundError, RuntimeError):
         pass
     safe_filesystem_op(os.symlink, src, dst)
 
